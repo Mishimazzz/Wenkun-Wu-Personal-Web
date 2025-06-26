440a1a01ccdab34c8d94bb2d25424a3e3afcc72b
yying8 <yueyan.ying0256@gmail.com>
2025-06-26 16:59:57 +0800

    update homePage and add animation cubes

--- vite-project/src/HomePage.tsx
+++ vite-project/src/HomePage.tsx
@@ -2,20 +2,28 @@
 import React from 'react'
 import Header from './Header'
 import "./HomePage.css"
+import IsometricBoxes from './IsometricBoxes';
+
 
 function HomePage() {
   return (
-    <div className='HomePage'>
+    <div>
       <Header />
-      <div className='leftSection'>
-        <h1>Software engineer, technical writer & open-source maintainer</h1>
-        <p>
-          I'm Victor Eke, an experienced frontend developer passionate about
-          learning and building open-source software that is beneficial to developers and the world at large.
-        </p>
+      <div className='wholeFrame'>
+      <div className='HomePage'>
+        <div className='leftSection'>
+          <h1>Software engineer, technical writer & open-source maintainer</h1>
+          <p>
+            I'm Victor Eke, an experienced frontend developer passionate about
+            learning and building open-source software that is beneficial to developers and the world at large.
+          </p>
+          {/* personal links */}
+          <a href='#'>LinkedIn</a>
+        </div>
+        <div className='rightSection'>
+          <IsometricBoxes />
+        </div>
       </div>
-      <div className='rightSection'>
-
       </div>
     </div>
   )
---
+++ vite-project/src/IsometricBoxes.tsx
@@ -0,0 +1,17 @@
+import './IsometricBoxes.css';
+
+export default function IsometricBoxes() {
+  return (
+    <div className="perspective">
+      <div className="cube"></div>
+      <div className="cube"></div>
+      <div className="cube"></div>
+      <div className="cube"></div>
+      <div className="cube"></div>
+      <div className="cube"></div>
+      <div className="cube"></div>
+      <div className="cube"></div>
+      <div className="cube"></div>
+    </div>
+  );
+}
