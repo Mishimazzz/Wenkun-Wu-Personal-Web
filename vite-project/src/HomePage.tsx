245a674102d820b6b7eecf1f3c8557d66771a866
yying8 <yueyan.ying0256@gmail.com>
2025-06-27 10:20:31 +0800

    update work experience css

--- vite-project/src/HomePage.tsx
+++ vite-project/src/HomePage.tsx
@@ -28,44 +28,50 @@ function HomePage() {
           </div>
         </div>
       </div>
+      <h1 className='work-header'>Work Experience</h1>
       <div className='BottomFrame'>
         <div className='workExperience'>
-          <h1>Work Experience</h1>
-          <WorkCard
-            logo="https://media.licdn.com/dms/image/v2/D4E0BAQG1TfjUTE2lcw/company-logo_100_100/company-logo_100_100/0/1708592321323/rhenus_logistics_logo?e=1756339200&amp;v=beta&amp;t=pl34Wt672SG4JwGCgLY3Mk0w4SjEFfHCSNybeSGDi0E"
-            companyName="Rhenus Logistics"
-            position="Inside Sales Support | Internship"
-            date="Jan 2025 – May 2025 | 5 mos"
-            introduction="My primary tasks at Aviyel included crafting technical guides and hosting coding sessions to bridge the gap with open-source content for some of the fastest-growing projects."
-          />
-          <WorkCard
-            logo="https://media.licdn.com/dms/image/v2/C4D0BAQERNrQoa9pguw/company-logo_100_100/company-logo_100_100/0/1631352787502?e=1756339200&amp;v=beta&amp;t=tkVPAzR6mYbvJxqNAlJvBxZ0b9XCG1q98HBxusTTgvM"
-            companyName="COSCO SHIPPING Lines (North America)"
-            position="Sales Assistant | Internship"
-            date="May 2024 – Aug 2024 | 4 mos"
-            introduction="My primary tasks at Aviyel included crafting technical guides and hosting coding sessions to bridge the gap with open-source content for some of the fastest-growing projects."
-          />
-          <WorkCard
-            logo="https://media.licdn.com/dms/image/v2/C4E0BAQHJVipjNd9UUQ/company-logo_100_100/company-logo_100_100/0/1657212585321?e=1756339200&amp;v=beta&amp;t=B7FZYiCoWGLcWcMiQNaQhUfuRQ-XPT8h96PeRlbvM2A"
-            companyName="Bakertilly CGSM"
-            position="Assistant Auditor | Internship"
-            date="Jan 2023 – Mar 2023 | 3 mos"
-            introduction="My primary tasks at Aviyel included crafting technical guides and hosting coding sessions to bridge the gap with open-source content for some of the fastest-growing projects."
-          />
-          <WorkCard
-            logo="/public/images.png"
-            companyName="Nanjing Nanshen Xidi Certified Public Accountant Co, Ltd"
-            position="Assistant Auditor | Internship"
-            date="Jan 2021 – Mar 2021 | 2 mos"
-            introduction="My primary tasks at Aviyel included crafting technical guides and hosting coding sessions to bridge the gap with open-source content for some of the fastest-growing projects."
-          />
-          <WorkCard
-            logo="https://media.licdn.com/dms/image/v2/C4E0BAQHFTX3sJscepw/company-logo_100_100/company-logo_100_100/0/1648551863411/_logo?e=1756339200&amp;v=beta&amp;t=LtesUz2pWzTzH44Bz3CHpkzvvsWhg9d26kPoHg1a9a0"
-            companyName="Suyin Financial Leasing Co Ltd"
-            position="Assistant Sales Manager | Internship"
-            date="Jul 2020 – Aug 2020 | 2 mos"
-            introduction="My primary tasks at Aviyel included crafting technical guides and hosting coding sessions to bridge the gap with open-source content for some of the fastest-growing projects."
-          />
+          
+          <div className='left-section'>
+            <WorkCard
+              logo="https://media.licdn.com/dms/image/v2/D4E0BAQG1TfjUTE2lcw/company-logo_100_100/company-logo_100_100/0/1708592321323/rhenus_logistics_logo?e=1756339200&amp;v=beta&amp;t=pl34Wt672SG4JwGCgLY3Mk0w4SjEFfHCSNybeSGDi0E"
+              companyName="Rhenus Logistics"
+              position="Inside Sales Support | Internship"
+              date="Jan 2025 – May 2025 | 5 mos"
+              introduction="My primary tasks at Aviyel included crafting technical guides and hosting coding sessions to bridge the gap with open-source content for some of the fastest-growing projects."
+            />
+            <WorkCard
+              logo="https://media.licdn.com/dms/image/v2/C4D0BAQERNrQoa9pguw/company-logo_100_100/company-logo_100_100/0/1631352787502?e=1756339200&amp;v=beta&amp;t=tkVPAzR6mYbvJxqNAlJvBxZ0b9XCG1q98HBxusTTgvM"
+              companyName="COSCO SHIPPING Lines (North America)"
+              position="Sales Assistant | Internship"
+              date="May 2024 – Aug 2024 | 4 mos"
+              introduction="My primary tasks at Aviyel included crafting technical guides and hosting coding sessions to bridge the gap with open-source content for some of the fastest-growing projects."
+            />
+            <WorkCard
+              logo="https://media.licdn.com/dms/image/v2/C4E0BAQHJVipjNd9UUQ/company-logo_100_100/company-logo_100_100/0/1657212585321?e=1756339200&amp;v=beta&amp;t=B7FZYiCoWGLcWcMiQNaQhUfuRQ-XPT8h96PeRlbvM2A"
+              companyName="Bakertilly CGSM"
+              position="Assistant Auditor | Internship"
+              date="Jan 2023 – Mar 2023 | 3 mos"
+              introduction="My primary tasks at Aviyel included crafting technical guides and hosting coding sessions to bridge the gap with open-source content for some of the fastest-growing projects."
+            />
+          </div>
+          
+          <div className='right-section'>
+            <WorkCard
+              logo="/public/images.png"
+              companyName="Nanjing Nanshen Xidi Certified Public Accountant Co, Ltd"
+              position="Assistant Auditor | Internship"
+              date="Jan 2021 – Mar 2021 | 2 mos"
+              introduction="My primary tasks at Aviyel included crafting technical guides and hosting coding sessions to bridge the gap with open-source content for some of the fastest-growing projects."
+            />
+            <WorkCard
+              logo="https://media.licdn.com/dms/image/v2/C4E0BAQHFTX3sJscepw/company-logo_100_100/company-logo_100_100/0/1648551863411/_logo?e=1756339200&amp;v=beta&amp;t=LtesUz2pWzTzH44Bz3CHpkzvvsWhg9d26kPoHg1a9a0"
+              companyName="Suyin Financial Leasing Co Ltd"
+              position="Assistant Sales Manager | Internship"
+              date="Jul 2020 – Aug 2020 | 2 mos"
+              introduction="My primary tasks at Aviyel included crafting technical guides and hosting coding sessions to bridge the gap with open-source content for some of the fastest-growing projects."
+            />
+          </div>
         </div>
       </div>
     </div>
