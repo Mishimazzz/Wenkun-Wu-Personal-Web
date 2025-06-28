d912bd55549a6cb93e42ea03c4535ab9c740c5b4
yying8 <yueyan.ying0256@gmail.com>
2025-06-28 20:22:34 +0800

    update font spacing and animation location

--- vite-project/src/Pages/HomePage.tsx
+++ vite-project/src/Pages/HomePage.tsx
@@ -15,7 +15,7 @@ function HomePage() {
             <h1>Software engineer, technical writer & open-source maintainer</h1>
             <p>
               I'm Victor Eke, an experienced frontend developer passionate about
-              learning and building open-source software that is beneficial to developers and the world at large.
+              learning and <br />building open-source software that is beneficial to developers and the world at large.
             </p>
             {/* personal links */}
               <a className="social-link" href="https://www.linkedin.com/in/wenkun-wu-20939b293/" target="_blank" rel="noopener noreferrer">
