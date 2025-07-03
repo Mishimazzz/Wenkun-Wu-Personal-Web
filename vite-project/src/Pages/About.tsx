258b79dfd7ca4e2878cf34bf65b807cb2eeeb10c
yying8 <yueyan.ying0256@gmail.com>
2025-07-03 16:32:38 +0800

    update personal photo

--- vite-project/src/Pages/About.tsx
+++ vite-project/src/Pages/About.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Header from '../Components/Header'
+import '../Pages/About.css'
 
 function About()
 {
@@ -10,15 +11,17 @@ function About()
       <div className='leftSection'>
         <h1>I'm Wenkun Wu. I live in Montreal, where I move containers and drive smarter supply chain decisions.</h1>
         <p>
-          I'm a data-driven supply chain specialist who happens to own a violin, a bass guitar,
-          and a gym membership (the last one is more symbolic than visible, thanks to my loyal baby fat).
-          Armed with advanced Excel skills and just enough Power BI to impress my manager,
-          I tackle pricing sheets and shipment mysteries by day, and string instruments by night.
-          On a totally unrelated note, I have an identical twin sister who’s my complete opposite —
-          she probably wouldn’t survive in logistics.
+          I'm a data-driven supply chain specialist who happens to own a violin, <br />
+          a bass guitar, and a gym membership (the last one is more symbolic than visible, <br />
+          thanks to my loyal baby fat). Armed with advanced Excel skills and just enough <br />
+          Power BI to impress my manager, I tackle pricing sheets and shipment mysteries by day, <br />
+          and string instruments by night. On a totally unrelated note, I have an identical twin sister <br />
+          who’s my complete opposite — she probably wouldn’t survive in logistics.
         </p>
       </div>
-      <div className='rightSection'></div>
+      <div className='rightSection'>
+          <img src="./public/wenkunwu.jpg" alt="wenkun wu image" />
+      </div>
       </div>
       
       <footer>
