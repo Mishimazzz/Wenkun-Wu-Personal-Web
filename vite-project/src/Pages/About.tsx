d24190104fb757f6e1d262fb4d51e4746b777152
yying8 <yueyan.ying0256@gmail.com>
2025-07-03 16:47:38 +0800

    update resume pdf and code in about.tsx

--- vite-project/src/Pages/About.tsx
+++ vite-project/src/Pages/About.tsx
@@ -20,7 +20,23 @@ function About()
         </p>
       </div>
       <div className='rightSection'>
-          <img src="./public/wenkunwu.jpg" alt="wenkun wu image" />
+        <img src="./public/wenkunwu.jpg" alt="wenkun wu image" />
+        <a href="">
+          View Résumé
+          <svg
+            stroke="currentColor"
+            fill="currentColor"
+            stroke-width="0"
+            viewBox="0 0 24 24"
+            height="1em"
+            width="1em"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="m13 3 3.293 3.293-7 7-1.414-1.414 7-7L21 11V3z"></path>
+            <path d="M19 19H5V5h7l-2-2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2h14c1.103 0 2-.897 2-2v-5l-2 2v3z"></path>
+          </svg>
+        </a>
+        <p>wenkunwu01@gmail.com</p>
       </div>
       </div>
       
